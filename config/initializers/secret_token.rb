fcda4cc40315b680faa75f95f3987319b357f608
App Academy Student <student@appacademy.io>
2014-06-24 09:48:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OkClone::Application.config.secret_key_base = 'ec011bbcf7e5a03a0a89676228634db6d9fd18fa7cf5c916779708a847d89759259ae3e6215f4a98a02e9e8922b2a6082c7ded16115a947f438de2102bad09d5'
