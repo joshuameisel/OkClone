b657c84ecfaf0f277d5fc942f162b9cd909ffdbb
Joshua Meisel <joshua.a.meisel@gmail.com>
2014-06-26 21:32:06 -0400

    style new session view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,88 @@
-Profile.create!([
-  {user_id: 1, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil},
-  {user_id: 3, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil},
-  {user_id: 4, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil},
-  {user_id: 5, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil},
-  {user_id: 6, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil},
-  {user_id: 2, summary: "just n LA gal tryin' to make it in the big City!", job: nil, fortes: nil, physical_forte: nil, likes: "\"The Bachelor\", the bachelor", six_things: nil, think_about: nil, friday_night: nil, most_private: "When I swim my fingies get pruny", religion: "Jew", ethnicity: nil, height: nil},
-  {user_id: 7, summary: nil, job: nil, fortes: nil, physical_forte: nil, likes: nil, six_things: nil, think_about: nil, friday_night: nil, most_private: nil, religion: nil, ethnicity: nil, height: nil}
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+  {
+    username: "josh",
+    gender: "m",
+    orientation: "straight",
+    dob: Date.new(1990, 2, 2),
+    country: "us",
+    zip_code: 11222,
+    email: "joshua.a.meisel@gmail.com",
+    password: "glitch"
+  },
+  
+  {
+    username: "simone",
+    gender: "f",
+    orientation: "straight",
+    min_age: 24,
+    max_age: 33,
+    dob: Date.new(1988, 12, 28),
+    country: "us",
+    zip_code: 90210,
+    email: "simone",
+    password: "glitch"
+  },
+  
+  {
+    username: "cougar",
+    gender: "f",
+    orientation: "straight",
+    dob: Date.new(1960, 12, 28),
+    country: "us",
+    zip_code: 90210,
+    email: "cougar@gmail.com",
+    password: "glitch"
+  },
+  
+  {
+    username: "jake",
+    gender: "m",
+    orientation: "straight",
+    min_age: 18,
+    max_age: 60,
+    dob: Date.new(1991, 5, 23),
+    country: "us",
+    zip_code: 11249,
+    email: "jacob",
+    password: "glitch"
+  },
+  
+  {
+    username: "bryan",
+    gender: "m",
+    orientation: "gay",
+    min_age: 20,
+    max_age: 26,
+    dob: Date.new(1991, 4, 16),
+    country: "us",
+    zip_code: 10002,
+    email: "bryan@gmail.com",
+    password: "glitch"
+  }, 
+  
+  {
+    username: "rod",
+    gender: "m",
+    orientation: "gay",
+    dob: Date.new(1989, 4, 16),
+    country: "us",
+    zip_code: 10031,
+    email: "rod@gmail.com",
+    password: "glitch"
+  }
 ])
-User.create!([
-  {username: "cougar", gender: "f", orientation: "straight", min_age: 18, max_age: 30, dob: "1960-12-28", country: "us", session_token: "_CfAdzJbJ5yBvZPUGtoGVg", zip_code: 90210, email: "cougar@gmail.com", password_digest: "$2a$10$nCLpHmaggiG52O5Kl/DLyOw6BJAkCZigtyygIp3O5uu/o2boNM2xm"},
-  {username: "jake", gender: "m", orientation: "straight", min_age: 18, max_age: 60, dob: "1991-05-23", country: "us", session_token: "5v4P9qylFOWv1XAL4o39_w", zip_code: 11249, email: "jacobappet@gmail.com", password_digest: "$2a$10$fNJvJ8VUWy0V8el4IBTBKOkq9AKe7OxwM83L1guQP1SW9nErnmiEe"},
-  {username: "rod", gender: "m", orientation: "gay", min_age: 18, max_age: 30, dob: "1989-04-16", country: "us", session_token: "dK7bDOCmlMgZU7VCKcygfg", zip_code: 10031, email: "rod@gmail.com", password_digest: "$2a$10$BQzhUYLTDZebf4VIy1Xfq.oZ.m39N4BHaWP02QZIlIYYMGmb8WcLK"},
-  {username: "simone", gender: "f", orientation: "straight", min_age: 24, max_age: 33, dob: "1988-12-28", country: "us", session_token: "sIldUn73-YoCNiTAGkoWDQ", zip_code: 90210, email: "sperrin28@gmail.com", password_digest: "$2a$10$D5BtugPK10e/6yxdS97gueOAWuRqUFUbcKM3zuUsz6cdrGtIanCPW"},
-  {username: "josh", gender: "m", orientation: "straight", min_age: 18, max_age: 30, dob: "1990-02-02", country: "us", session_token: "jMb_20OLo8Mqp3Swf98KVw", zip_code: 11222, email: "joshua.a.meisel@gmail.com", password_digest: "$2a$10$RD/ik8oh8li30Kc1HUxEYueFCA5D54raTvrdmAME1C3iAkYlgPcCe"},
-  {username: "bryan", gender: "m", orientation: "gay", min_age: 20, max_age: 26, dob: "1991-04-16", country: "us", session_token: "HAywpqaV5VMM_1XMW7fxdg", zip_code: 10002, email: "bryan@gmail.com", password_digest: "$2a$10$1IUk9XhBw0sTP0nb7aCqeu2qxr/o80el6H4AsUVIae9MFgqoQarqG"},
-  {username: "jorge", gender: "m", orientation: "straight", min_age: 18, max_age: 30, dob: "1991-01-26", country: "us", session_token: "lSSGEhHr2GQi6Z_VGfECvg", zip_code: 10003, email: "jorge@app.com", password_digest: "$2a$10$m4K/Xp.6GrGXw9Lqo/4tI.CDVs90yXt9lr3JkmjMzcofTWtBl7M1e"}
-])
+
+Profile.find(2).update({
+  religion: "Jew",
+  summary: "Just an LA gal tryin' to make it in the big City!",
+  likes: "\"The Bachelor,\" the bachelor",
+  most_private: "When I swim my fingies get pruny"
+})
