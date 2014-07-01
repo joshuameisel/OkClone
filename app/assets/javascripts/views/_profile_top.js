433068a404d2f9e71130b6d8cd682d739a03dc48
App Academy Student <student@appacademy.io>
2014-07-01 10:22:47 -0400

    implement html for edit user info form

--- app/assets/javascripts/views/_profile_top.js
+++ app/assets/javascripts/views/_profile_top.js
@@ -2,15 +2,15 @@ OkClone.Views.ProfileTop = Backbone.View.extend({
   template: JST["users/profile_top"],
 	tagName: "section",
 	className: "profile-top",
-	
+
 	initialize: function () {
 		this.$el.addClass("group");
 	},
-	
+
   render: function () {
     var renderedContent = this.template({user: this.model})
     this.$el.html(renderedContent)
-    
+
     return this
   }
 })
--- app/controllers/api/profiles_controller.rb
+++
@@ -1,2 +0,0 @@
-def show
-end
