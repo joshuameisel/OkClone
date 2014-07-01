a83c4d98d26a390e58fda4c98ae81432009d1340
Joshua Meisel <joshua.a.meisel@gmail.com>
2014-07-01 01:27:25 -0400

    Add edit button to one's own profile

--- app/assets/javascripts/views/_profile_top.js
+++ app/assets/javascripts/views/_profile_top.js
@@ -1,5 +1,12 @@
 OkClone.Views.ProfileTop = Backbone.View.extend({
   template: JST["users/profile_top"],
+	tagName: "section",
+	className: "profile-top",
+	
+	initialize: function () {
+		this.$el.addClass("group");
+	},
+	
   render: function () {
     var renderedContent = this.template({user: this.model})
     this.$el.html(renderedContent)
--- app/assets/javascripts/views/users_show.js
+++ app/assets/javascripts/views/users_show.js
@@ -1,12 +1,19 @@
 OkClone.Views.UserShow = Backbone.View.extend({
   template: JST["users/show"],
-  className: "profile",
-  initialize: function () {
+  className: ((this.ownProfile) ? "profile editable" : "profile"),
+  initialize: function (options) {
     this.subViews = [];
+		this.ownProfile = options.ownProfile;
+		if (this.ownProfile) {
+			this.$el.addClass("editable");
+		}
   },
   
   render: function () {
-    var topView = new OkClone.Views.ProfileTop({model: this.model});
+    var topView = new OkClone.Views.ProfileTop({
+			model: this.model, 
+			ownProfile: this.ownProfile
+		});
     this.$el.html(topView.render().$el)
     
     return this
