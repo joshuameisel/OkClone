7a55eee48c4eb3718f7d88e9baf36180ee0c0f87
App Academy Student <student@appacademy.io>
2014-07-07 13:06:44 -0400

    show correct show_me on user search when search_params present

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def search_text
+    {show_me: ["Girls", "Guys", "Guys and girls"]}
+  end
+
+  def show_me(num)
+    params[:search] ? params[:search][:show_me].to_i == num : nil
+  end
 end
