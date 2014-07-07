998e500459be34fe58cca33444b51495b74a6ee9
App Academy Student <student@appacademy.io>
2014-07-07 13:41:21 -0400

    implement which user search who_like is selected

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,9 @@
 module UsersHelper
   def search_text
-    {show_me: ["Girls", "Guys", "Guys and girls"]}
+    {
+      show_me: ["Girls", "Guys", "Guys and girls"],
+      who_like: ["girls", "guys", "either"]
+    }
   end
 
   def show_me(num)
@@ -8,6 +11,14 @@ module UsersHelper
       params[:search][:show_me].to_i == num
     else
       current_user.show_me_value == num
+    end
+  end
+
+  def who_like(num)
+    if params[:search]
+      params[:search][:who_like].to_i == num
+    else
+      User::GENDERS.invert[[current_user.gender]] == num
     end
   end
 end
