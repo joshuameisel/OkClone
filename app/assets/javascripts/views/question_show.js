bc5f0295b9df0c7aedca0350c47886571b545383
Joshua Meisel <joshua.a.meisel@gmail.com>
2014-07-16 03:21:15 -0400

    fix bug caused by asynchronous multiple uses of same variable at different values

--- app/assets/javascripts/views/question_show.js
+++ app/assets/javascripts/views/question_show.js
@@ -4,13 +4,14 @@ QuestionShow = OkClone.Views.QuestionShow = Backbone.View.extend({
 	className: "group question",
 
   initialize: function (options) {
-    this.listenTo(this.model, "change", this.render);
+
   },
 
   render: function () {
     var renderedContent = this.template({
     	currentUser: this.model.get("current_user"), 
-			user: this.model.get("user")
+			user: this.model.get("user"),
+			question: this.model.get("question")
     });
     this.$el.html(renderedContent);
 
