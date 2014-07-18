a27c86d8f95d1404d8636ab916b7767d58000823
Josh Meisel <joshua.a.meisel@gmail.com>
2014-07-18 09:31:22 -0400

    display mismatched answers in red

--- app/assets/javascripts/views/answer_show.js
+++ app/assets/javascripts/views/answer_show.js
@@ -21,11 +21,40 @@ AnswerShow = OkClone.Views.AnswerShow = Backbone.View.extend({
     this.listenTo(this.model, "sync", this.render);
   },
 
+  mismatch: function () {
+    var mismatch = {};
+    var user = this.model.get("user");
+    var currentUser = this.model.get("current_user");
+    if (user && currentUser) {
+      var isMatch = false;
+      currentUser.acceptable_answers.forEach(function(acc_answer) {
+        if (acc_answer.answer_choice_id == user.answer.id) {
+          isMatch = true;
+        }
+      });
+      if (!isMatch) {
+        mismatch.user = true;
+      }
+
+      isMatch = false;
+      user.acceptable_answers.forEach(function(acc_answer) {
+        if (acc_answer.answer_choice_id == currentUser.answer.id) {
+          var isMatch = true;
+        }
+      });
+      if (!isMatch) {
+        mismatch.currentUser = true;
+      }
+    }
+    return mismatch;
+  },
+
   render: function () {
     var renderedContent = this.template({
     	currentUser: this.model.get("current_user"),
 			user: this.model.get("user"),
-			question: this.model.get("question")
+			question: this.model.get("question"),
+      mismatch: this.mismatch()
     });
     this.$el.html(renderedContent);
 
